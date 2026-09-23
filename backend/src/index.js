7e34e1a2d65ed46225a6e3a0b2ae79dff7ee4469
Dinsanda Amajith <damajithrajapaksha@gmail.com>
2026-09-23 13:08:15 +0530

    feat: implement authentication routes and middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ import job from './lib/cron.js';
 import { clerkMiddleware } from '@clerk/express';
 import cors from 'cors';
 import clerkWebhook from './webhooks/clerk.webhook.js';
+import authRoutes from './routes/auth.route.js';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -25,6 +26,8 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "ok" });
 });
+
+app.use("api/auth", authRoutes)
 
 // if the public directory exists, serve the static files
 // this is for the production build
